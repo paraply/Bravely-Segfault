bfbcf6d7bdc6c2e26f4b11e591c323121b2da3d8
Anton Josefsson <gusjosanl@student.gu.se>
2016-04-19 19:04:22 +0200

    Added some empty stuff for character class

--- src/main/java/Model/GameObjects/Character.java
+++ src/main/java/Model/GameObjects/Character.java
@@ -1,5 +1,7 @@
 package Model.GameObjects;
 
+import Model.World;
+
 /**
  * Created by Anton on 2016-04-19.
  */
@@ -7,10 +9,24 @@ public class Character extends GameObject {
     private int screenX;
     private int screenY;
 
+    private int targetX;
+    private int targetY;
+    private float speed;
+
+
     public int getScreenX(){return screenX;}
     public int getScreenY(){return screenY;}
 
     public Character(int x, int y) {
         super(x, y);
+        screenX = x * World.tileSize;
+        screenY = y * World.tileSize;
+        targetX = screenX;
+        targetY = screenY;
+    }
+
+    public void Update()
+    {
+        
     }
 }
