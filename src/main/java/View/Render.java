df40c579135453b214e12c659bd5701a853f7a35
Mikael <git@evinja.se>
2016-04-19 17:02:24 +0200

    Added a render class. Removed temporary file

---
+++ src/main/java/View/Render.java
@@ -0,0 +1,26 @@
+package View;
+
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ * Created by paraply on 2016-04-19.
+ */
+public class Render {
+    private static Render render;
+    private GraphicsContext gc;
+
+    // graphics context = main-canvas context
+    // this is set by WindowController in its initialization
+    public void setGraphicsContext(GraphicsContext gc){
+        this.gc = gc;
+    }
+
+    //This class is currently singleton, since its instance needs to be accessed by both WindowController and the Model. May change...
+    public static synchronized Render getInstance(){
+        if (render == null){
+            render = new Render();
+        }
+        return render;
+    }
+
+}
