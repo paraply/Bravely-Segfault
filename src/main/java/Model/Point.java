ce5fb18e14920e1c500441bd1116b866eb9de3fa
Stefan Fritzon <stefan.fritzon@gmail.com>
2016-04-19 21:08:37 +0200

    Created Point with test development, works in NetBeans

---
+++ src/main/java/Model/Point.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import lombok.Getter;
+
+
+
+/**
+ *
+ * @author Admin
+ */
+public class Point {
+
+    private final int x;
+    private final int y;
+
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+        }
+    
+    public int getX(){
+        return this.x;
+    }
+    
+    
+    public int getY(){
+        return this.y;
+    }
+}
