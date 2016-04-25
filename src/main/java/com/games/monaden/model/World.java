c1d8ddc01df138bcd71c6eb79065505ce902af2c
Anton Josefsson <gusjosanl@student.gu.se>
2016-04-25 23:22:37 +0200

    Objects with collision are now in a list instead of checked by tile-basis

--- src/main/java/com/games/monaden/model/World.java
+++ src/main/java/com/games/monaden/model/World.java
@@ -1,21 +1,24 @@
 package com.games.monaden.model;
 
+import Model.GameObjects.GameObject;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Anton on 2016-04-17.
  * There should only be a single instance of this class, but it should not have a global access point.
  * For now only sending a message to the console, should be handled in a better way
  */
 public class World{
+    private static boolean instantiated = false;
 
     public static final int tileSize = 32;
     public static final int mapSize = 16;
 
+    private List<GameObject> objects = new ArrayList<GameObject>();
+
     //Temporarily hardcoded here, should always load a map from a file
-    //Currently hardcoded to have size 8*10 with magic numbers, should be changed ASAP!
     private int[][] tileMap = new int[mapSize][mapSize];
-
-    private static boolean instantiated = false;
-
     public int[][] getTileMap(){
         return tileMap;
     }
@@ -44,6 +47,14 @@ public class World{
                 {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
                 {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
         };
+
+        for(int y = 0; y < mapSize; y++) {
+            for(int x = 0; x < mapSize; x++) {
+                if(CheckSolidTile(tileMap[y][x])){
+                    objects.add(new GameObject(new Point(x,y)));
+                }
+            }
+        }
     }
 
     public enum MovementDirection {
@@ -69,10 +80,11 @@ public class World{
                 break;
         }
         if(newPoint.getY() < 0 || newPoint.getY() >= mapSize
-                || newPoint.getX() < 0 || newPoint.getX() >= mapSize
-                || CheckSolidTile(tileMap[newPoint.getY()][newPoint.getX()]))
+                || newPoint.getX() < 0 || newPoint.getX() >= mapSize)
             return p;
-
+        for(GameObject g : objects) {
+            if(g.getPosition().equals(newPoint)) return p;
+        }
         return newPoint;
     }
 
