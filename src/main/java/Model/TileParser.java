33cfd3e85a1b1e6ddd574071738a92b1958b5e20
th4m <ph.th4m@gmail.com>
2016-04-25 18:16:18 +0200

    Implemented a parser for parsing tiles from XML-files

--- src/main/java/Model/TileParser.java
+++ src/main/java/Model/TileParser.java
@@ -27,17 +27,20 @@ public class TileParser extends DefaultHandler {
     public void startElement (String uri, String localName, String qName,
                               Attributes attributes) throws SAXException {
         if (qName.equalsIgnoreCase("tile")) {
-
             id = Integer.parseInt(attributes.getValue("id"));
         } else if (qName.equalsIgnoreCase("name")) {
-            System.out.println("Name found: " + localName);
             bName = true;
         } else if (qName.equalsIgnoreCase("graphics")) {
-            System.out.println("Filepath found: " + localName);
             bGraphics = true;
         } else if (qName.equalsIgnoreCase("solidness")) {
-            System.out.print("Solidness found: " + localName);
             bSolid = true;
+        }
+    }
+
+    @Override
+    public void endElement (String uri, String localName, String qName) throws SAXException{
+        if (qName.equalsIgnoreCase("tile")) {
+            createTile();
         }
     }
 
@@ -45,26 +48,37 @@ public class TileParser extends DefaultHandler {
     public void characters (char ch[], int start, int length) throws SAXException {
         if (bName) {
             this.name = new String(ch, start, length);
+//            System.out.println("Name found: " + name);
             bName = false;
         } else if (bGraphics) {
             this.filepath = new File(new String (ch, start, length));
+//            System.out.println("Filepath found: " + filepath);
             bGraphics = false;
         } else if (bSolid) {
             if ("solid".equalsIgnoreCase(new String(ch, start, length))) {
                 this.solidness = true;
             }
+//            System.out.println("Solidness found: " + solidness);
+            bSolid = false;
         }
     }
 
     /**
-     * Should be called after parsing
-     * @return
+     * Should be called after fully parsing one tile
      */
-    public void createTile () {
+    private void createTile () {
         try {
-            tileList.add(new Tile(name, solidness, filepath));
+            tileList.add(new Tile(id, name, solidness, filepath));
         } catch (NullPointerException e) {
             System.err.println("Something was missing when creating a tile");
         }
+    }
+
+    /**
+     * Should be called after fully parsing all tiles from a file
+     * @return the list of all parsed tiles
+     */
+    public List<Tile> getTiles () {
+        return tileList;
     }
 }
