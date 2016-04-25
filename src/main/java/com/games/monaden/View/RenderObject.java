aa6abb012c6f088ddb2ab533de3d4c612d1d0ada
Mikael <git@evinja.se>
2016-04-25 17:36:48 +0200

    Added comments to RenderObject. Renamed some methods to make it easier to understand.

--- src/main/java/com/games/monaden/View/RenderObject.java
+++ src/main/java/com/games/monaden/View/RenderObject.java
@@ -5,33 +5,52 @@ import com.games.monaden.Model.World;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
+
 /**
  * Created by paraply on 2016-04-21.
+ *
+ * This class draws objects without animations on the screen.
+ * It is a superclass to AnimatedObject that handles animations.
+ *
+ * There should only be one instance of this class that is handled by Render.
  */
-public class RenderObject {
-    protected GameObject gameObject;
-    protected GraphicsContext context;
-    protected Image image;
 
-    protected final int IMAGE_HEIGHT = 32, IMAGE_WIDTH = 32; // Currently hardcoded. Could possibly be specified in XML.
-    protected int x,y; // Objects position in the world
-    protected int image_src_X, image_src_Y; // Where from the the tileset should be used as source.
+class RenderObject {
+    GameObject gameObject;
+    GraphicsContext context;
+    Image image;
+
+    final int IMAGE_HEIGHT = 32, IMAGE_WIDTH = 32; // Currently hardcoded. Could possibly be specified in XML.
+    int x,y; // Objects position in the world
+    int image_src_X, image_src_Y; // Coordinates to get a specific picture from the tileset
 
 
-    public RenderObject(GameObject gameObject, GraphicsContext graphicsContext, String imageSection, String imageName){
+    // Create a new instance of the RenderObject
+    // A RenderObject must know where to draw = graphicsContext
+    // It must know which imageSection to get the image such as "Characters"
+    // It must have a imageFile which is accessed in the imageSection folder in resources,
+    RenderObject(GameObject gameObject, GraphicsContext graphicsContext, String imageSection, String imageName){
         this.gameObject = gameObject;
         context = graphicsContext;
         image = new Image( imageSection + "/" + imageName + ".png");
     }
 
 
-    private void calculateX(){
-        image_src_X = 0;
+    // x,y values specifies where in the world the character should be drawn
+    private void calculateWorldCoordinates(){
         x = gameObject.getX() * World.tileSize;
         y =  gameObject.getY() * World.tileSize;
     }
 
-    protected void calculateY(){
+    // The X value from the source image is always 0 in an RenderObject without animation.
+    private void calculateSourceX(){
+        image_src_X = 0;
+    }
+
+    // The Y value depends on which direction the object is facing.
+    // If the object is stationary then it will default to downwards
+    // and use the top image from the tileset.
+    void calculateSourceY(){
         switch (gameObject.getDirection()){
             case UP:
                 image_src_Y = IMAGE_HEIGHT * 3;
@@ -47,9 +66,11 @@ public class RenderObject {
         }
     }
 
+    // Calculate coordinates and draw to context
     public void draw(){
-        calculateX();
-        calculateY();
+        calculateWorldCoordinates();
+        calculateSourceX();
+        calculateSourceY();
         context.drawImage(image, image_src_X,image_src_Y, IMAGE_HEIGHT, IMAGE_HEIGHT, x, y, IMAGE_HEIGHT, IMAGE_HEIGHT);
     }
 }
