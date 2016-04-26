46fab95442f5854b80c2a5bc315e3e14f96b60e6
th4m <ph.th4m@gmail.com>
2016-04-26 17:05:52 +0200

    Renamed files

---
+++ src/main/java/com/games/monaden/model/TileMapParser.java
@@ -0,0 +1,29 @@
+package com.games.monaden.model;
+
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+/**
+ * Created by Philip on 2016-04-26.
+ *
+ */
+public class TileMapParser extends DefaultHandler {
+
+
+    @Override
+    public void startElement (String uri, String localName, String qName,
+                              Attributes attributes) throws SAXException {
+
+    }
+
+    @Override
+    public void endElement (String uri, String localName, String qName) throws SAXException{
+
+    }
+
+    @Override
+    public void characters (char ch[], int start, int length) throws SAXException {
+
+    }
+}
