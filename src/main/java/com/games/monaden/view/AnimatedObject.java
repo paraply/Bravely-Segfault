85aa99328b773f682534be091881cb4be49fb805
Mikael <git@evinja.se>
2016-04-27 12:28:46 +0200

    cleaning up AnimationObject and RenderObject

--- src/main/java/com/games/monaden/view/AnimatedObject.java
+++ src/main/java/com/games/monaden/view/AnimatedObject.java
@@ -15,22 +15,19 @@ import javafx.scene.canvas.GraphicsContext;
 class AnimatedObject extends RenderObject {
     private Point previousPosition;
 
-//    private int inTransition = 0; // Used for moving objects. This will handle movement from previous position to the new one. If 0 then the object has not yet moved or is finished moving.
+    // *** Variables used for animations ***
+    private int animationTicks = 0;             // The animations cannot run at the same frequency as the GameLoop frequency. It would be to fast.
+                                                // This will loop 0 1 2 3 ... ANIMATION_FREQUENCY
+                                                // When reached ANIMATION_FREQUENCY-value then animate.
+    private final int ANIMATION_FREQUENCY = 8;  // Which frequency that a new animation frame will be drawn. The higher number the slower animation.
+    private final int ANIMATION_FRAMES = 2;     // How many pictures X-wise in the animation tileset. Could possibly be specified in XML later. Remember that this is counted from ZERO!
+    private int currentAnimationFrame = 0;      // We need to keep track of which animation frame we have rendered
 
 
-
-
-
-    private int animation_part = 0;
-
-
-
-    private int currentAnimationFrame = 0;
-
-    private final int TRANSITION_SPEED = GameLoop.FREQUENCY == 32 ? 1 : 2 ; // Transition speed should be 2 if GameLoop frequency is 16. Should Be 1 if GameLoop frequency is 32.
-    private final int ANIMATION_SPEED = 3; // Uses a mod of the inTransition number.
-    private final int REAL_ANIMATION_SPEED = ANIMATION_SPEED * TRANSITION_SPEED;
-
+    // ** Variables used for transitioning **
+    private int currentTransitionStep = 0;  // 0 = object is not moving or has finished moving to the new coordinate
+                                             // Otherwise the object is currently moving to another coordinate.
+    private final int PIXELS_PER_STEP = 2;  // If we move a pixel at a time it will be to slow. This can be adjusted here.
 
     AnimatedObject(GameObject gameObject) {
         super(gameObject);
@@ -43,21 +40,6 @@ class AnimatedObject extends RenderObject {
         drawToContext();
     }
 
-    // *** Variables used for animations ***
-    private int animationTicks = 0;     // The animations cannot run at the same frequency as the GameLoop frequency. It would be to fast.
-                                        // This will loop 0 1 2 3 ... ANIMATION_FREQUENCY
-                                        // When reached ANIMATION_FREQUENCY-value then animate.
-    private final int ANIMATION_FREQUENCY = 8; // Which frequency that a new animation frame will be drawn. The higher number the slower animation.
-    private final int ANIMATION_FRAMES = 2; // How many pictures X-wise in the animation tileset. Could possibly be specified in XML later. Remember that this is counted from ZERO!
-
-
-
-    // ** Variables used for transitioning **
-    private int currentTransitionStep = 0;  // 0 = object is not moving or has finished moving to the new coordinate
-                                            // Otherwise the object is currently moving to another coordinate.
-    private final int PIXELS_PER_STEP = 2;  // If we move a pixel at a time it will be to slow. This can be adjusted here.
-
-
     private void calculateSourceX(){
 
         if (gameObject.hasContinuousAnimation()){ // This is used for objects that are not moving but still has an animation.
@@ -68,11 +50,11 @@ class AnimatedObject extends RenderObject {
             animationTick(); // Animate
 
         }else{ // Else if object does not have a continuous animation. Then it should only be animated during transition.
-            if (currentTransitionStep == 0){ // We are currently not in a moving state
+            if (currentTransitionStep == 0){                                // We are currently not in a moving state
                 if (!gameObject.getPosition().equals(previousPosition)){    // Check if we should be in a moving state (e.g the objects coordinates has changed since last time)
-                    currentTransitionStep = World.tileSize; // If we have a 32-bit tileSize, then we should move to another tile in maximum 32 steps.
-                    image_src_X = 0; // Do not animate first. We want to change the objects direction this time.
-                    x = previousPosition.getX() * World.tileSize; // Stand still for now, we want to change direction first. Start moving in next transition.
+                    currentTransitionStep = World.tileSize;                 // If we have a 32-bit tileSize, then we should move to another tile in maximum 32 steps.
+                    image_src_X = 0;                                        // Do not animate first. We want to change the objects direction this time.
+                    x = previousPosition.getX() * World.tileSize;           // Stand still for now, we want to change direction first. Start moving in next transition.
                     y =  previousPosition.getY() * World.tileSize;
                 }else{ // Object is standing still
                     image_src_X = 0;
@@ -85,13 +67,13 @@ class AnimatedObject extends RenderObject {
                 currentTransitionStep = currentTransitionStep - PIXELS_PER_STEP; // We move a specific number of pixels
 
             switch (gameObject.getDirection()){ //
-                case UP: y =  (gameObject.getPosition().getY() * World.tileSize) + currentTransitionStep; // Move up step by step
+                case UP: y =  (gameObject.getPosition().getY() * World.tileSize) + currentTransitionStep;       // Move up step by step
                     break;
-                case DOWN: y =  (gameObject.getPosition().getY() * World.tileSize)   - currentTransitionStep; // Move down step by step
+                case DOWN: y =  (gameObject.getPosition().getY() * World.tileSize)   - currentTransitionStep;   // Move down step by step
                     break;
-                case LEFT: x =  (gameObject.getPosition().getX() * World.tileSize) +  currentTransitionStep; // Move left step by step
+                case LEFT: x =  (gameObject.getPosition().getX() * World.tileSize) +  currentTransitionStep;    // Move left step by step
                     break;
-                case RIGHT: x =  (gameObject.getPosition().getX() * World.tileSize)  - currentTransitionStep; // Move right step by step
+                case RIGHT: x =  (gameObject.getPosition().getX() * World.tileSize)  - currentTransitionStep;   // Move right step by step
                     break;
             }
 
@@ -100,13 +82,8 @@ class AnimatedObject extends RenderObject {
             }
 
             }
-
         }
-
     }
-
-
-
 
     private void animationTick(){
         animationTicks++;
@@ -119,45 +96,9 @@ class AnimatedObject extends RenderObject {
             }else{
                 currentAnimationFrame++;                    // Draw the next frame this time
             }
-
             image_src_X = currentAnimationFrame * gameObject.getWidth();
         }
     }
 
 
-//    private void calculateSourceX(){
-//        if (inTransition == 0){ // Is currently not animating and moving
-//            if (!gameObject.getPosition().equals(previousPosition)){
-//                inTransition = World.tileSize; // Start transition
-//                image_src_X = 0;
-//                x = previousPosition.getX() * World.tileSize; // Stand still for now, we want to change direction first. Start moving in next transition.
-//                y =  previousPosition.getY() * World.tileSize;
-//            }else{ // Object is standing still
-//                image_src_X = 0;
-//                x = gameObject.getPosition().getX() * World.tileSize;
-//                y =  gameObject.getPosition().getY() * World.tileSize;
-//            }
-//        }else{
-//            inTransition -=TRANSITION_SPEED;
-//            image_src_X = (animation_part % ANIMATION_FRAMES) * gameObject.getWidth();
-//            if ((World.tileSize - inTransition) % REAL_ANIMATION_SPEED == 0){
-//                animation_part++;
-//            }
-//            switch (gameObject.getDirection()){
-//                case UP: y =  (gameObject.getPosition().getY() * World.tileSize) + inTransition; // Move up step by step
-//                    break;
-//                case DOWN: y =  (gameObject.getPosition().getY() * World.tileSize)   - inTransition; // Move down step by step
-//                    break;
-//                case LEFT: x =  (gameObject.getPosition().getX() * World.tileSize) +  inTransition; // Move left step by step
-//                    break;
-//                case RIGHT: x =  (gameObject.getPosition().getX() * World.tileSize)  - inTransition; // Move right step by step
-//                    break;
-//            }
-//
-//            if (inTransition == 0){ // transition is done now
-//                previousPosition = gameObject.getPosition();
-//            }
-//        }
-//
-//    }
 }
