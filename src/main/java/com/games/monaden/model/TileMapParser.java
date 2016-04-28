da02cb09cfd3e622d178125fa03a3cf6ce94ab0c
th4m <ph.th4m@gmail.com>
2016-04-28 11:22:22 +0200

    Started writing some parsing, but the branch needs to rebase master first

--- src/main/java/com/games/monaden/model/TileMapParser.java
+++ src/main/java/com/games/monaden/model/TileMapParser.java
@@ -1,5 +1,6 @@
 package com.games.monaden.model;
 
+import com.games.monaden.model.gameObjects.Character;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
@@ -10,11 +11,59 @@ import org.xml.sax.SAXException;
  */
 public class TileMapParser extends DefaultHandler {
 
+    private boolean bLine = false;
+    private boolean bCharName = false;
+    private boolean bCharPos = false;
+    private boolean bGraphics = false;
+    private boolean bFile = false;
+    private boolean bFrame = false;
+    private boolean bDialogue = false;
+
+    private String charName;
+    private Point charPos;
+
 
     @Override
     public void startElement (String uri, String localName, String qName,
                               Attributes attributes) throws SAXException {
+        switch (qName.toLowerCase()) {
 
+            case "line":
+                bLine = true;
+                break;
+
+            case  "characters":
+            //TODO: New character?
+                break;
+
+            case "character":
+                charName = attributes.getValue("name");
+                break;
+
+            case "position":
+                bCharPos = true;
+                break;
+
+            case "graphics":
+                bGraphics = true;
+                break;
+
+            case "fileName":
+                bFile = true;
+                break;
+
+            case "frameCount":
+                bFrame = true;
+                break;
+
+            case "dialogue":
+                bDialogue = true;
+                break;
+
+            case "transition":
+                //TODO: ???
+                break;
+        }
     }
 
     @Override
