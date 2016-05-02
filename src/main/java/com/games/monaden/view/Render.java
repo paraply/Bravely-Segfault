a318eb1e559e49edc70cec16b8ee23f2b6039c90
Anton Josefsson <gusjosanl@student.gu.se>
2016-05-02 23:27:22 +0200

    Added Render as observer

--- src/main/java/com/games/monaden/view/Render.java
+++ src/main/java/com/games/monaden/view/Render.java
@@ -7,11 +7,13 @@ import javafx.scene.image.Image;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observer;
+import java.util.Observable;
 
 /**
  * Created by paraply on 2016-04-19.
  */
-public class Render {
+public class Render implements Observer{
     private static Render render; // Used by getInstance
     private GraphicsContext context;
     private World world; // World model provides information about what should be drawn
@@ -86,4 +88,20 @@ public class Render {
         context.drawImage(images[index], tile_x * World.tileSize , tile_y * World.tileSize);
     }
 
+    @Override
+    public void update(Observable observable, Object arg)
+    {
+        //Should probably be refactored later
+        if(observable == world) {
+            if(arg == "transition"){
+                for (GameObject go : world.getObjects()){
+                    if (go.hasContinuousAnimation()){
+                        objects.add(new AnimatedObject(go));
+                    }else{
+                        objects.add(new RenderObject(go));
+                    }
+                }
+            }
+        }
+    }
 }
