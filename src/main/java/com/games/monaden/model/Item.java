8c834bb855d035574630ec515b7397eaed419051
Stefan Fritzon <stefan.fritzon@gmail.com>
2016-05-04 10:10:10 +0200

    Item class is ready, Inventory is on it's way

--- src/main/java/com/games/monaden/model/Item.java
+++ src/main/java/com/games/monaden/model/Item.java
@@ -7,19 +7,32 @@ public class Item {
 
     private boolean consumable = false;
     private boolean keyItem = false;
+    private final String description;
 
     public Item(String description) {
+        this.description = description;
     }
 
     public void setConsumable(boolean consumable) {
+
         this.consumable = consumable;
     }
 
     public void setKeyItem(boolean keyItem) {
+
         this.keyItem = keyItem;
     }
 
     public boolean getConsumable() {
+
         return consumable;
+    }
+
+    public boolean getKeyItem() {
+        return keyItem;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
