add1a8d24952b8624af42ce6b1d6e9b264164d0f
Anton Josefsson <gusjosanl@student.gu.se>
2016-05-15 16:23:15 +0200

    Removed comment

--- src/main/java/com/games/monaden/model/World.java
+++ src/main/java/com/games/monaden/model/World.java
@@ -69,7 +69,6 @@ public class World extends Observable{
 
             interactables = levelParser.getInteractables();
             transitions = levelParser.getTransitions();
-            System.out.println(transitions.get(0).newPos);
             objects.clear();
             //Loop through the tilemap and create tiles for each
             for (int y = 0; y < MAP_SIZE; y++) {
