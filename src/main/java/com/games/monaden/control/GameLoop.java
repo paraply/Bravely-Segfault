674536a135d24d19d2adde8da56f22665ff172f9
SpockReko <stefan.fritzon@gmail.com>
2016-05-18 12:37:12 +0200

    Revert "changed all 5** to 0** tiles id's"
    This reverts commit 9da850f50ba3971a824884fdd83864ef67bde53e.

--- src/main/java/com/games/monaden/control/GameLoop.java
+++ src/main/java/com/games/monaden/control/GameLoop.java
@@ -20,7 +20,7 @@ public class GameLoop extends AnimationTimer {
     CharacterController playerCharacter;
 
     public void initializeGame(){
-        world = new World("smallroommonaden.xml" );
+        world = new World("second.xml" );
         Render.getInstance().setWorld(world);
         playerCharacter = new CharacterController();
     }
